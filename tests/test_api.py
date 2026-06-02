544cdf9f3f3996f3fe375fdac42097acd775c9b7
Venko Venkov <150695843+vmvenkov@users.noreply.github.com>
2026-06-02 14:42:44 +0300

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,1 +1,40 @@
+from fastapi.testclient import TestClient
+from app.main import app
 
+client = TestClient(app)
+
+
+def test_home():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert "message" in response.json()
+
+
+def test_health():
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.json()["status"] == "ok"
+
+
+def test_predict_valid_data():
+    response = client.post("/predict", json={
+        "sepal_length": 5.1,
+        "sepal_width": 3.5,
+        "petal_length": 1.4,
+        "petal_width": 0.2
+    })
+
+    assert response.status_code == 200
+    assert "prediction" in response.json()
+    assert "class_name" in response.json()
+
+
+def test_predict_invalid_data():
+    response = client.post("/predict", json={
+        "sepal_length": -1,
+        "sepal_width": 3.5,
+        "petal_length": 1.4,
+        "petal_width": 0.2
+    })
+
+    assert response.status_code == 422
