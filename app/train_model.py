6384b362807d3c1b01237053c0ef9eefea317667
Venko Venkov <150695843+vmvenkov@users.noreply.github.com>
2026-06-02 14:37:05 +0300

    Update train_model.py

--- app/train_model.py
+++ app/train_model.py
@@ -1,1 +1,15 @@
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+import os
 
+X, y = load_iris(return_X_y=True)
+
+model = RandomForestClassifier(random_state=42)
+model.fit(X, y)
+
+os.makedirs("app/models", exist_ok=True)
+
+joblib.dump(model, "app/models/iris_model.pkl")
+
+print("Model saved.")
