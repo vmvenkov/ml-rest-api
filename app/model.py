a4c9680e89a55b600a16904664cb488b0e6686e4
Venko Venkov <150695843+vmvenkov@users.noreply.github.com>
2026-06-02 14:38:21 +0300

    Update model.py

--- app/model.py
+++ app/model.py
@@ -1,1 +1,10 @@
+import joblib
+import os
 
+MODEL_PATH = "app/models/iris_model.pkl"
+
+def load_model():
+    if not os.path.exists(MODEL_PATH):
+        raise FileNotFoundError("Model file not found.")
+
+    return joblib.load(MODEL_PATH)
